828ade29b33139c4e5a75bfe5a0661522523c68e
Mike Torres <apolloproductionsdj@gmail.com>
2019-06-25 03:21:10 +0000

    carrierwave not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_151104) do
+ActiveRecord::Schema.define(version: 2019_06_24_060832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_06_13_151104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picutre"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
