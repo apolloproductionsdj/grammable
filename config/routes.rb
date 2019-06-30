fa50585c6e039fa23001c3578e348fa7812251d5
Mike Torres <apolloproductionsdj@gmail.com>
2019-06-30 06:57:54 +0000

    need deprecation error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :models
   root "grams#index" # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :grams
-
+  resources :grams do 
+  resources :comments, only: :create
+ end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,25 +3,26 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
   describe 'grams#create action'
     it "should allow users to create comments on grams" do
-      gram = Factorybot.create(:gram)
+      gram = FactoryBot.create(:gram)
+
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram_id: gram.id, comment: { message: "awesome gram" } }
 
       expect(response).to redirect_to root_path
       expect(gram.comments.length).to eq 1
-      expect(gram.comment.first.message).to eq "awesome gram"
+      expect(gram.comments.first.message).to eq "awesome gram"
     end 
 
     it "should require a user to be logged in to comment on a gram" do 
-      gram = Factorybot.create(:gram)
-
+      gram = FactoryBot.create(:gram)
       post :create, params: { gram_id: gram.id, comment: { message: "awesome gram" } }
       expect(response).to redirect_to new_user_session_path
     end 
 
     it "should return http status code of not found if the gram isn't found" do 
-      user = Factorybot.create(:user)
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram_id: 'YOLOSWAG', comment: { message: "awesome gram" } }
